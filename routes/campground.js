c5a6474278b54f891fa44bae62ab591e7dbfba38
Laurence Ho <bluegray.he@aol.com>
2017-02-06 15:24:14 +1300

    modify campground route for using Sqlite3

--- routes/campground.js
+++ routes/campground.js
@@ -1,82 +1,65 @@
-var db = require('../db');
+var db = require('../db/sqlite');
 var express = require('express');
 var router = express.Router();
 var middleware = require('../middleware/index');
 
 
 // get all camp grounds
-router.get('/campground', function(req, res) {
-    db.getConnection((err, connection) => {
+router.get('/campground', function (req, res) {
+    db.all('SELECT * FROM campgrounds', function (err, rows) {
         if (err) {
             req.flash('error', err);
         } else {
-            connection.query('SELECT * FROM campgrounds', function(err, results, fields) {
-                connection.release();
-
-                if (err) {
-                    req.flash('error', err);
-                } else {
-                    res.render('campgrounds/index', { campgrounds: results });
-                }
-            });
+            res.render('campgrounds/index', { campgrounds: rows });
         }
     });
 });
 
 // get create camp ground form
-router.get('/campground/new', middleware.isLoggedIn, function(req, res) {
+router.get('/campground/new', middleware.isLoggedIn, function (req, res) {
     res.render('campgrounds/new');
 });
 
 // create one camp ground
-router.post('/campground', middleware.isLoggedIn, function(req, res) {
-    req.body.campground.name = req.sanitize(req.body.campground.name);
-    req.body.campground.image = req.sanitize(req.body.campground.image);
-    req.body.campground.description = req.sanitize(req.body.campground.description);
+router.post('/campground', middleware.isLoggedIn, function (req, res) {
+    var user_id = req.body.campground.user_id;
+    var username = req.body.campground.username;
 
-    db.getConnection((err, connection) => {
+    var name = req.sanitize(req.body.campground.name);
+    var image = req.sanitize(req.body.campground.image);
+    var desc = req.sanitize(req.body.campground.description);
+
+    var sql = "INSERT INTO campgrounds (name, image, description, user_id, username) VALUES ('" +
+        name + "','" + image + "','" + desc + "','" + user_id + "','" + username + "')";
+
+    db.run(sql, function (err, result) {
         if (err) {
             req.flash('error', err);
-            res.render('campgrounds/new');
         } else {
-            connection.query('INSERT INTO campgrounds SET ?', req.body.campground, function(err, result) {
-                connection.release();
-
-                if (err) {
-                    req.flash('error', err);
-                } else {
-                    console.log('Create new campground:' + req.body.campground.name);
-                    res.redirect('/campground');
-                }
-            });
+            res.redirect('/campground');
         }
     });
 });
 
 // get one camp ground with comments
-router.get('/campground/:id', function(req, res) {
+router.get('/campground/:id', function (req, res) {
     var campground = [],
         comments = [];
 
-    db.getConnection((err, connection) => {
+    var campQuery = 'SELECT * FROM campgrounds WHERE id = ' + req.params.id;
+    var commentQuery = 'SELECT * FROM comments WHERE campground_id = ' + req.params.id;
+
+    db.all(campQuery, function (err, rows) {
         if (err) {
             req.flash('error', err);
         } else {
-            connection.query('SELECT * FROM campgrounds WHERE id = ?', [req.params.id], function(err, result, fields) {
+            campground = rows[0];
+
+            db.all(commentQuery, function (err, rows) {
                 if (err) {
                     req.flash('error', err);
                 } else {
-                    campground = result[0];
-                }
-            });
-
-            connection.query('SELECT * FROM comments WHERE campground_id = ?', [req.params.id], function(err, result, fields) {
-                connection.release();
-
-                if (err) {
-                    req.flash('error', err);
-                } else {
-                    comments = result;
+                    comments = rows;
 
                     res.render('campgrounds/one', {
                         campground: campground,
@@ -89,63 +72,51 @@ router.get('/campground/:id', function(req, res) {
 });
 
 // get edit camp ground form
-router.get('/campground/:id/edit', middleware.checkCampOwner, function(req, res) {
-    db.getConnection((err, connection) => {
+router.get('/campground/:id/edit', middleware.checkCampOwner, function (req, res) {
+    var campQuery = 'SELECT * FROM campgrounds WHERE id = ' + req.params.id;
+
+    db.all(campQuery, function (err, rows) {
         if (err) {
             req.flash('error', err);
-            res.redirect('/campground' + req.params.id);
+            res.redirect('/campground/' + req.params.id);
         } else {
-            connection.query('SELECT * FROM campgrounds WHERE id = ?', [req.params.id], function(err, result, fields) {
-                connection.release();
-
-                if (err) {
-                    req.flash('error', err);
-                    res.redirect('/campground/' + req.params.id);
-                } else {
-                    res.render('campgrounds/edit', { campground: result[0] });
-                }
-            });
+            res.render('campgrounds/edit', { campground: rows[0] });
         }
     });
 });
 
 // edit one camp ground
-router.put('/campground/:id', middleware.checkCampOwner, function(req, res) {
-    req.body.campground.name = req.sanitize(req.body.campground.name);
-    req.body.campground.image = req.sanitize(req.body.campground.image);
-    req.body.campground.description = req.sanitize(req.body.campground.description);
+router.put('/campground/:id', middleware.checkCampOwner, function (req, res) {
+    var name = req.sanitize(req.body.campground.name);
+    var image = req.sanitize(req.body.campground.image);
+    var desc = req.sanitize(req.body.campground.description);
 
-    db.getConnection((err, connection) => {
+    var updateCampSQL = "UPDATE campgrounds SET " +
+        "name = '" + name + "'," +
+        "image = '" + image + "'," +
+        "description = '" + desc + "'" +
+        " WHERE id = " + req.params.id;
+
+    db.run(updateCampSQL, function (err, rows) {
         if (err) {
             req.flash('error', err);
-            res.redirect('/campground/' + req.params.id);
+            res.redirect('/campground');
         } else {
-            connection.query('UPDATE campgrounds SET ? WHERE id = ?', [req.body.campground, req.params.id], function(err, result, fields) {
-                connection.release();
-
-                if (err) {
-                    req.flash('error', err);
-                    res.redirect('/campground');
-                } else {
-                    res.redirect('/campground/' + req.params.id);
-                }
-            });
+            res.redirect('/campground/' + req.params.id);
         }
+
     });
 });
 
 // delete one camp ground
-router.delete('/campground/:id', middleware.checkCampOwner, function(req, res) {
-    db.getConnection((err, connection) => {
+router.delete('/campground/:id', middleware.checkCampOwner, function (req, res) {
+    var deleteCampSQL = 'DELETE FROM campgrounds WHERE id = ' + req.params.id;
+    db.run(deleteCampSQL, function (err) {
         if (err) {
             req.flash('error', err);
             res.redirect('/campground' + req.params.id);
         } else {
-            connection.query('DELETE FROM campgrounds WHERE id = ?', [req.params.id], function(err, result, fields) {
-                connection.release();
-
-                res.redirect('/campground');
-            });
+            res.redirect('/campground');
         }
     });
 });
