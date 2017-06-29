476f0cae9b45ff003625f4428e153a89f7aed9b5
Laurence Ho <bluegray.he@aol.com>
2017-06-29 18:48:14 +1200

    reformat

--- server/passport.js
+++ server/passport.js
@@ -4,110 +4,110 @@ var bcrypt = require('bcrypt-nodejs');
 var db = require('./sqlite');
 
 module.exports = function (passport) {
-    // =========================================================================
-    // passport session setup ==================================================
-    // =========================================================================
-    // required for persistent login sessions
-    // passport needs ability to serialize and unserialize users out of session
+	// =========================================================================
+	// passport session setup ==================================================
+	// =========================================================================
+	// required for persistent login sessions
+	// passport needs ability to serialize and unserialize users out of session
 
-    // used to serialize the user for the session
-    passport.serializeUser(function (user, done) {
-        done(null, user.username);
-    });
+	// used to serialize the user for the session
+	passport.serializeUser(function (user, done) {
+		done(null, user.username);
+	});
 
-    // used to deserialize the user
-    passport.deserializeUser(function (username, done) {
-        var query = "SELECT * FROM users WHERE username = '" + username + "'";
-        db.all(query, function (err, rows) {
-            if (err) {
-                req.flash('error', err);
-            } else {
-                done(err, rows[0]);
-            }
-        });
-    });
+	// used to deserialize the user
+	passport.deserializeUser(function (username, done) {
+		var query = "SELECT * FROM users WHERE username = '" + username + "'";
+		db.all(query, function (err, rows) {
+			if (err) {
+				req.flash('error', err);
+			} else {
+				done(err, rows[0]);
+			}
+		});
+	});
 
-    // =========================================================================
-    // LOCAL SIGNUP ============================================================
-    // =========================================================================
-    // we are using named strategies since we have one for login and one for signup
-    // by default, if there was no name, it would just be called 'local'
+	// =========================================================================
+	// LOCAL SIGNUP ============================================================
+	// =========================================================================
+	// we are using named strategies since we have one for login and one for signup
+	// by default, if there was no name, it would just be called 'local'
 
-    passport.use('local-signup',
-        new LocalStrategy({
-            // by default, local strategy uses username and password, we will override with email
-            usernameField: 'username',
-            passwordField: 'password',
-            passReqToCallback: true // allows us to pass back the entire request to the callback
-        },
-            function (req, username, password, done) {
-                console.log('---- User Signup ----');
-                console.log('username:' + username);
+	passport.use('local-signup',
+		new LocalStrategy({
+				// by default, local strategy uses username and password, we will override with email
+				usernameField: 'username',
+				passwordField: 'password',
+				passReqToCallback: true // allows us to pass back the entire request to the callback
+			},
+			function (req, username, password, done) {
+				console.log('---- User Signup ----');
+				console.log('username:' + username);
 
-                var query = "SELECT * FROM users WHERE username = '" + username + "'";
+				var query = "SELECT * FROM users WHERE username = '" + username + "'";
 
-                db.all(query, function (err, rows) {
-                    if (err) {
-                        return done(err);
-                    }
+				db.all(query, function (err, rows) {
+					if (err) {
+						return done(err);
+					}
 
-                    if (rows.length) {
-                        return done(null, false, req.flash('error', 'That username is already taken.'));
-                    } else {
-                        // if there is no user with that username, create the user
-                        var newUserMysql = {
-                            username: username,
-                            password: bcrypt.hashSync(password, null, null) // use the generateHash function in our user model
-                        };
+					if (rows.length) {
+						return done(null, false, req.flash('error', 'That username is already taken.'));
+					} else {
+						// if there is no user with that username, create the user
+						var newUserMysql = {
+							username: username,
+							password: bcrypt.hashSync(password, null, null) // use the generateHash function in our user model
+						};
 
-                        var insertQuery = "INSERT INTO users ( username, password ) values ('" + newUserMysql.username + "','" + newUserMysql.password + "')";
+						var insertQuery = "INSERT INTO users ( username, password ) values ('" + newUserMysql.username + "','" + newUserMysql.password + "')";
 
-                        db.run(insertQuery, function (err) {
-                            if (err) {
-                                return done(err);
-                            }
-                            return done(null, newUserMysql);
-                        });
-                    }
-                });
-            })
-    );
+						db.run(insertQuery, function (err) {
+							if (err) {
+								return done(err);
+							}
+							return done(null, newUserMysql);
+						});
+					}
+				});
+			})
+	);
 
-    // =========================================================================
-    // LOCAL LOGIN =============================================================
-    // =========================================================================
-    // we are using named strategies since we have one for login and one for signup
-    // by default, if there was no name, it would just be called 'local'
+	// =========================================================================
+	// LOCAL LOGIN =============================================================
+	// =========================================================================
+	// we are using named strategies since we have one for login and one for signup
+	// by default, if there was no name, it would just be called 'local'
 
-    passport.use('local-login',
-        new LocalStrategy({
-            // by default, local strategy uses username and password, we will override with email
-            usernameField: 'username',
-            passwordField: 'password',
-            passReqToCallback: true // allows us to pass back the entire request to the callback
-        },
-            function (req, username, password, done) { // callback with email and password from our form
-                console.log('---- User login ----');
-                console.log('username:' + username);
+	passport.use('local-login',
+		new LocalStrategy({
+				// by default, local strategy uses username and password, we will override with email
+				usernameField: 'username',
+				passwordField: 'password',
+				passReqToCallback: true // allows us to pass back the entire request to the callback
+			},
+			function (req, username, password, done) { // callback with email and password from our form
+				console.log('---- User login ----');
+				console.log('username:' + username);
 
-                var query = "SELECT * FROM users WHERE username = '" + username + "'";
+				var query = "SELECT * FROM users WHERE username = '" + username + "'";
 
-                db.all(query, function (err, rows) {
-                    if (err) {
-                        return done(err);
-                    }
+				db.all(query, function (err, rows) {
+					if (err) {
+						return done(err);
+					}
 
-                    if (!rows.length) {
-                        return done(null, false, req.flash('error', 'No user found.'));
-                    } else {
-                        // if the user is found but the password is wrong
-                        if (!bcrypt.compareSync(password, rows[0].password)) {
-                            return done(null, false, req.flash('error', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
-                        } else { // all is well, return successful user
-                            return done(null, rows[0], req.flash('success', 'Welcome to YelpCamp ' + username));
-                        }
-                    }
-                });
-            })
-    );
+					if (!rows.length) {
+						return done(null, false, req.flash('error', 'No user found.'));
+					} else {
+						// if the user is found but the password is wrong
+						if (!bcrypt.compareSync(password, rows[0].password)) {
+							return done(null, false, req.flash('error', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+						} else { // all is well, return successful user
+							return done(null, rows[0], req.flash('success', 'Welcome to YelpCamp ' + username));
+						}
+					}
+				});
+			})
+	);
 };
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,35 +4,33 @@ var passport = require('passport');
 var middleware = require('../middleware');
 
 router.post('/api/login', passport.authenticate('local-login', {
-        successRedirect: '/profile', // redirect to the secure profile section
-        failureRedirect: '/login', // redirect back to the signup page if there is an error
-        failureFlash: true // allow flash messages
-    }),
-    function(req, res) {
-        if (req.body.remember) {
-            req.session.cookie.maxAge = 1000 * 60 * 3;
-        } else {
-            req.session.cookie.expires = false;
-        }
-        res.redirect('/');
-    }
+		successRedirect: '/profile', // redirect to the secure profile section
+		failureRedirect: '/login', // redirect back to the signup page if there is an error
+		failureFlash: true // allow flash messages
+	}),
+	function (req, res) {
+		if (req.body.remember) {
+			req.session.cookie.maxAge = 1000 * 60 * 3;
+		} else {
+			req.session.cookie.expires = false;
+		}
+		res.redirect('/');
+	}
 );
 
 router.post('/api/signup', passport.authenticate('local-signup', {
-    successRedirect: '/profile', // redirect to the secure profile section
-    failureRedirect: '/signup', // redirect back to the signup page if there is an error
-    failureFlash: true // allow flash messages
+	successRedirect: '/profile', // redirect to the secure profile section
+	failureRedirect: '/signup', // redirect back to the signup page if there is an error
+	failureFlash: true // allow flash messages
 }));
 
-router.get('/profile', middleware.isLoggedIn, function(req, res) {
-    res.render('profile', {
-        user: req.user // get the user out of session and pass to template
-    });
+router.get('/api/profile', middleware.isLoggedIn, function (req, res) {
+	res.json(req.user);
 });
 
-router.get('/api/logout', function(req, res) {
-    req.logout();
-    res.redirect('/');
+router.get('/api/logout', function (req, res) {
+	req.logout();
+	res.redirect('/');
 });
 
 module.exports = router;
