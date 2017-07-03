4ce7e2814351ca4dd55cdd9b0c8a83fd72495cb3
Laurence Ho <bluegray.he@aol.com>
2017-07-04 11:05:29 +1200

    clean up

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var session = require('express-session');
 var expressSanitizer = require('express-sanitizer');
 var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
 var passport = require('passport');
 var path = require('path');
 
@@ -12,7 +11,6 @@ require('./passport')(passport);
 
 var app = express();
 
-app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
